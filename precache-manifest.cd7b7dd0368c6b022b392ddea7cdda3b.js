09bc7a7b39be0485c569e202559a3cc1b45cc2bd
Jesse <459659168@qq.com>
2020-01-09 15:47:18 +0800

    Updates

---
+++ precache-manifest.cd7b7dd0368c6b022b392ddea7cdda3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d63b05f7c5236cc7b93283440dece62",
+    "url": "/mini-blog-admin/index.html"
+  },
+  {
+    "revision": "7d8ed9def8fb73f6f446",
+    "url": "/mini-blog-admin/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "07f68d3de7460c35fca8",
+    "url": "/mini-blog-admin/static/css/main.cd0894b5.chunk.css"
+  },
+  {
+    "revision": "7d8ed9def8fb73f6f446",
+    "url": "/mini-blog-admin/static/js/2.8b8a2a8c.chunk.js"
+  },
+  {
+    "revision": "6f546ee2b96c86c26970f4c33e77cb7e",
+    "url": "/mini-blog-admin/static/js/2.8b8a2a8c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07f68d3de7460c35fca8",
+    "url": "/mini-blog-admin/static/js/main.a1c775e9.chunk.js"
+  },
+  {
+    "revision": "5c654747fe4577ba9b26",
+    "url": "/mini-blog-admin/static/js/runtime-main.1be9885c.js"
+  }
+]);
